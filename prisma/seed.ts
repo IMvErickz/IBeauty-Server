7987f1ed6444c31a85a820b2ec2493d862f935ca
Erick dos Santos <erickspy2003@gmail.com>
2023-06-21 22:58:46 -0300

    test seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,12 +2,11 @@ import { randomUUID } from "crypto";
 import { prisma } from "../server/lib/prisma";
 
 async function Seed() {
-    await prisma.service.update({
-        where: {
-            id: '4ebc7532-2d9b-408a-bbea-31a503ef44f8'
-        },
+    await prisma.category.create({
         data: {
-            img: "https://www.segredosdesalao.com.br/dw/image/v2/AAFM_PRD/on/demandware.static/-/Sites-segredosdesalao-br-Library/default/dw2520b93c/images/blog/blog-article/247194-esta-pensando-em-adotar-um-corte-fade-c-orig-3.jpg?sw=480&q=70"
+            id: randomUUID(),
+            NameCategory: "Teste",
+            description: "testando..."
         }
     })
 }
