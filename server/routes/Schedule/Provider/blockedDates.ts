028672cf425d00521a72da7cc91176893b8945a8
IMvErickz <erickspy2003@gmail.com>
2024-05-20 21:13:13 -0300

    feat: route to get blocked-date

---
+++ server/routes/Schedule/Provider/blockedDates.ts
@@ -0,0 +1,60 @@
+import { FastifyInstance } from "fastify";
+import { prisma } from "../../../lib/prisma";
+import { z } from "zod";
+
+const blockedDateSchema = z.object({
+    id: z.string()
+})
+
+const blockedDateQueryParams = z.object({
+    year: z.number(),
+    month: z.number()
+})
+
+
+export async function BlockedDates(fastify: FastifyInstance) {
+    fastify.get('/blocked-date/:id', async (request) => {
+
+        const { id } = blockedDateSchema.parse(request.params)
+        const { year, month } = blockedDateQueryParams.parse(request.query)
+
+        const availableWeekDays = await prisma.timeInterval.findMany({
+            select: {
+              week_day: true,
+            },
+            where: {
+                Provider: {
+                  id,
+              }
+            },
+          })
+
+        const blockedWeekDays = [0, 1, 2, 3, 4, 5, 6].filter((weekDay) => {
+            return !availableWeekDays.some(
+              (availableWeekDay) => availableWeekDay.week_day === weekDay,
+            )
+          })
+        
+        const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
+            SELECT
+              EXTRACT(DAY FROM S.date) AS date,
+              COUNT(S.date) AS amount,
+              ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
+            FROM "Scheduling" S
+            LEFT JOIN "TimeInterval" UTI
+              ON UTI.week_day = (EXTRACT(DOW FROM S.date) + 1) % 7
+
+              WHERE S.user_id = ${id}
+                AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+
+            GROUP BY
+              EXTRACT(DAY FROM S.date),
+              ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+            HAVING COUNT(S.date) >= ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60);
+  `;
+        
+        const blockedDates = blockedDatesRaw.map((item) => item.date)
+
+        return {blockedDates, blockedWeekDays}
+    })
+}
